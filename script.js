90da90483f20f670ab926b452d2b8b1ec3bdd40c
Dylan <dylandixon@hotmail.com>
2021-04-11 00:06:54 -0230

    Create oneRound function, added comments

--- script.js
+++ script.js
@@ -15,6 +15,34 @@ function computerPlay() {
     }
 }
 
-function oneRound() {
-    
+/*oneRound: takes the parameters, compares them, then returns a result
+based on the winner */
+function oneRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toUpperCase();
+    computerSelection = computerSelection.toUpperCase();
+
+    const computerWinMessage = `Computer wins! ${computerSelection} beats ${playerSelection}`;
+    const playerWinMessage = `You win! ${playerSelection} beats ${computerSelection}`;
+    const tieMessage = 'It\'s a tie!';
+
+    if(playerSelection === computerSelection){
+        return tieMessage;
+    }
+    else if(
+        (playerSelection === 'ROCK' && computerSelection === 'SCISSORS') || 
+        (playerSelection === 'SCISSORS' && computerSelection === 'PAPER') ||
+        (playerSelection === 'PAPER' && computerSelection === 'ROCK')
+    ){
+        return playerWinMessage;
+    }
+    else if(
+        (computerSelection === 'ROCK' && playerSelection === 'SCISSORS') || 
+        (computerSelection === 'SCISSORS' && playerSelection === 'PAPER') ||
+        (computerSelection === 'PAPER' && playerSelection === 'ROCK')
+    ){
+        return computerWinMessage; 
+    }
+    else{
+        return 'Please enter valid parameters.'
+    }
 }
